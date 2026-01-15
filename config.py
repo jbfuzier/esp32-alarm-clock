20e9f05fa456b894b64afdbfe65b4f245194838b
jbfuzier <jbfuzier@gmail.com>
2026-01-15 11:43:26 +0100

    Create config.py with WiFi and alarm settings

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
-# Configuration parameters for ESP32 Alarm Clock
-
-# LED configuration
-LED_PIN = 2  # Example GPIO pin for LED
-LED_BRIGHTNESS = 255  # Brightness level (0-255)
-
-# WiFi configuration
-WIFI_SSID = 'your_wifi_ssid'
-WIFI_PASSWORD = 'your_wifi_password'
-
-# NTP configuration
+SSID = 'your_wifi_ssid'
+PASSWORD = 'your_wifi_password'
 NTP_SERVER = 'pool.ntp.org'
-NTP_UPDATE_INTERVAL = 3600  # Update time every hour
-
-# Alarm configuration
-ALARM_TIME = '07:00'  # Default alarm time (HH:MM)
-ALARM_ENABLED = True  # Set to False to disable the alarm
+RETRY_ATTEMPTS = 3
+INTERVAL = 5
+LED_PIN = 25
+LED_COUNT = 60
+LED_BRIGHTNESS_MAX = 255
+WARM_WHITE_RGB = (255, 200, 150)
+COLD_WHITE_RGB = (255, 255, 255)
+ALARM_CHECK_INTERVAL = 1  # seconds
+DEFAULT_RAMP_DURATION = 30  # minutes
+DEFAULT_MAX_BRIGHTNESS = 80
+ERROR_BLINK_INTERVAL = 500  # ms
+ALARMS_FILE = 'alarms.json'
+SETTINGS_FILE = 'settings.json'
+WEB_SERVER_PORT = 80
+WEB_SERVER_HOST = '0.0.0.0'
+TIMEZONE_OFFSET = '+1'
+DAYLIGHT_SAVING_OFFSET = 1
